55f95b057079dc41cc4cc1ef9c50e82aa159e0b1
Alexander Merkulov <sasha@merqlove.ru>
2014-07-11 03:08:18 +0400

    Typo fix

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,7 +3,4 @@ when "redhat", "centos", "amazon", "oracle"
   default['duplicity_ng']['install_method'] = "package" # Can be "source" or "package"
   default['duplicity_ng']['rpm']['checksum'] = "e89c866b6f3fb0be6b84637cb9d09af5"
   default['duplicity_ng']['rpm']['url'] = "https://launchpad.net/duplicity/0.6-series/0.6.24/+download/duplicity-0.6.24-0.fdr.6.i386.rpm"
-else
-  default['duplicity_ng']['rpm']['checksum'] = ""
-  default['duplicity_ng']['rpm']['url'] = ""
 end
