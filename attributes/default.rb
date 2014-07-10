de84392dfefe1b32222275ba6a6cb455dc52865c
Alexander Merkulov <sasha@merqlove.ru>
2014-07-11 03:02:32 +0400

    Added attar's for remote RPM

---
+++ attributes/default.rb
@@ -0,0 +1,8 @@
+case node['platform']
+when "redhat", "centos", "amazon", "oracle"
+  default['duplicity_ng']['rpm']['checksum'] = "e89c866b6f3fb0be6b84637cb9d09af5"
+  default['duplicity_ng']['rpm']['url'] = "https://launchpad.net/duplicity/0.6-series/0.6.24/+download/duplicity-0.6.24-0.fdr.6.i386.rpm"
+else
+  default['duplicity_ng']['rpm']['checksum'] = ""
+  default['duplicity_ng']['rpm']['url'] = ""
+end
