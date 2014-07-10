4c786071e886ba32d26fa7d1b53fa297abb6fb8f
Alexander Merkulov <sasha@merqlove.ru>
2014-07-11 03:07:45 +0400

    Typo fix

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,6 @@
 case node['platform']
 when "redhat", "centos", "amazon", "oracle"
+  default['duplicity_ng']['install_method'] = "package" # Can be "source" or "package"
   default['duplicity_ng']['rpm']['checksum'] = "e89c866b6f3fb0be6b84637cb9d09af5"
   default['duplicity_ng']['rpm']['url'] = "https://launchpad.net/duplicity/0.6-series/0.6.24/+download/duplicity-0.6.24-0.fdr.6.i386.rpm"
 else
