58a277eb1b4d0af459720dd92f9ac879fd49a7d2
Alexander Merkulov <sasha@merqlove.ru>
2014-07-16 01:07:12 +0400

    added integration tests, fixed python-dev/devel loading for nodes with python installed from packages

---
+++ files/default/tests/minitest/install_source_test.rb
@@ -0,0 +1,31 @@
+#
+# Cookbook Name:: duplicity_ng
+# Test:: install_package
+#
+# Copyright (C) 2014 Alexander Merkulov
+# Copyright (C) 2014 Chris Aumann
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+require File.expand_path('../support/helpers', __FILE__)
+
+describe 'duplicity_ng::install' do
+  include Helpers::TestHelper
+
+  it 'installs duplicity package from source' do
+    file(which('duplicity')).must_exist
+  end
+
+end
---
+++ files/default/tests/minitest/support/helpers.rb
@@ -0,0 +1,41 @@
+#
+# Cookbook Name:: duplicity_ng
+#
+# Copyright (C) 2014 Alexander Merkulov
+# Copyright (C) 2014 Chris Aumann
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+module Helpers
+  module TestHelper
+    require 'chef/mixin/shell_out'
+    include Chef::Mixin::ShellOut
+    include MiniTest::Chef::Assertions
+    include MiniTest::Chef::Context
+    include MiniTest::Chef::Resources
+
+    # courtesy http://stackoverflow.com/questions/2108727/which-in-ruby-checking-if-program-exists-in-path-from-ruby
+    def which(cmd)
+      exts = ENV['PATHEXT'] ? ENV['PATHEXT'].split(';') : ['']
+      ENV['PATH'].split(File::PATH_SEPARATOR).each do |path|
+        exts.each { |ext|
+          exe = File.join(path, "#{cmd}#{ext}")
+          return exe if File.executable? exe
+        }
+      end
+      return nil
+    end
+  end
+end
