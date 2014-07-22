5700c206e1dee53e320fb113a7ee5ad28d886c43
Chris Aumann <me@chr4.org>
2014-07-22 15:36:17 +0200

    Add header to attributes/default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,24 @@
+#
+# Cookbook Name:: duplicity_ng
+# Attributes:: default
+#
+# Copyright (C) 2014 Chris Aumann
+# Copyright (C) 2014 Alexander Merkulov
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 default['duplicity_ng']['install_method'] = 'package' # Can be "source" or "package"
 default['duplicity_ng']['binary'] = '/usr/bin/duplicity'
 default['duplicity_ng']['source']['use_pip'] = false # If true then we try to use pip
