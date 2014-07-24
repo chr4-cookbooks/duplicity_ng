8cf440238ac5c389bbcb83e7c56772c0db9b5e0a
Chris Aumann <me@chr4.org>
2014-07-24 10:32:33 +0200

    Fix indention in metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -5,5 +5,4 @@ license          'GNU Public License 3.0'
 description      'Installs/Configures duplicity_ng'
 long_description 'Installs/Configures duplicity_ng'
 version          '0.2.2'
-
-depends 'python'
+depends          'python'
