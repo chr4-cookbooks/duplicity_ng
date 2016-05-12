9beef5c91792523e22f2cd74d415e7659442f640
Chris Aumann <me@chr4.org>
2016-05-12 18:37:15 +0200

    Add source_url and issues_url to metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,8 @@ maintainer_email 'me@chr4.org'
 license          'GNU Public License 3.0'
 description      'Installs/Configures duplicity_ng'
 long_description 'Installs/Configures duplicity_ng'
+source_url       'https://github.com/chr4-cookbooks/duplicity_ng' if respond_to?(:source_url)
+issues_url       'https://github.com/chr4-cookbooks/duplicity_ng/issues' if respond_to?(:issues_url)
 version          '1.2.5'
 depends          'python'
 depends          'build-essential'
